b610c91c087ec68354f2f81b9b7c95ef28685826
kenten132 <darthcorbin@gmail.com>
2017-07-11 15:18:54 -0400

    Debugging.

--- java/rapcap/rapcap-lib/src/main/java/rapcap/lib/PriorityQueue.java
+++ java/rapcap/rapcap-lib/src/main/java/rapcap/lib/PriorityQueue.java
@@ -3,7 +3,7 @@ package rapcap.lib;
 public class PriorityQueue {
 	long priorityQueue[];
 	int zero = 0;
-	
+
 	PriorityQueue(long values[]) {
 		priorityQueue = new long[values.length];
 		System.arraycopy(values, 0, priorityQueue, 0, values.length);
@@ -39,30 +39,31 @@ public class PriorityQueue {
 		int change = (priorityQueue.length - zero) / 2;
 		
 		while (change > 0) {
+
+
+			// determine if we should insert at this position
+			if (pos == (priorityQueue.length - 1)
+				|| ((priorityQueue[pos]   >> 32) < value_shifted
+						&& (priorityQueue[pos+1] >> 32) > value_shifted))
+				break;
+
+			if (value > priorityQueue[pos]) {
+				// go right
+				pos = pos + change - (change >> 1);
+				//(int)(priorityQueue.length - pos) / 2;
+			}
+			else {
+				// go left
+				pos = pos - change + (change >> 1); //(int)(pos - zero + 1) / 2;
+			}
+
+			change >>= 1;
 			// handle equality, update without insertion
 			if ((priorityQueue[pos] >> 32) == value_shifted) {
 				// update
 				priorityQueue[pos] = value;
 				return;
 			}
-
-			// determine if we should insert at this position
-			if (pos == priorityQueue.length
-			 || ((priorityQueue[pos]   >> 32) < value_shifted
-			  && (priorityQueue[pos+1] >> 32) > value_shifted))
-				break;
-
-			if (value > priorityQueue[pos]) {
-				// go right
-				pos += change >> 1;
-				//(int)(priorityQueue.length - pos) / 2;
-			}
-			else {
-				// go left
-				pos = pos - change + (change>>1); //(int)(pos - zero + 1) / 2;
-			}
-
-			change >>= 1;
 		}
 
 		// add space for new value
@@ -74,7 +75,9 @@ public class PriorityQueue {
 		
 		// insert the value
 		priorityQueue[pos] = value;
-		if (priorityQueue[pos-1] >= priorityQueue[pos] || priorityQueue[pos] >= priorityQueue[pos+1])
-			System.out.println(pos + " " + (priorityQueue.length - zero) + "\n" + (priorityQueue[pos-1]>>32) + "\n" + (priorityQueue[pos]>>32) + "\n" + (priorityQueue[pos+1]>>32));
+//		if (priorityQueue[pos-1] >= priorityQueue[pos] || priorityQueue[pos] >= priorityQueue[pos+1])
+//			System.out.println(pos + " " + (priorityQueue.length - zero) + "\n" + (priorityQueue[pos-1]>>32) + "\n" + (priorityQueue[pos]>>32) + "\n" + (priorityQueue[pos+1]>>32));
+//		if ((priorityQueue[pos] >> 32) == (priorityQueue[pos+1 >> 32]) || (priorityQueue[pos] >> 32) == (priorityQueue[pos-1] >> 32))
+//			System.out.println("Duplicate entries found.");
 	}
 }
--- java/rapcap/rapcap-lib/src/main/java/rapcap/lib/SolutionSet.java
+++ java/rapcap/rapcap-lib/src/main/java/rapcap/lib/SolutionSet.java
@@ -20,7 +20,7 @@ public class SolutionSet {
 		long value = minheap.pop();
 		s.last_index = (int)((value << 32) >> 32);
 		s.next_index = (int)(value >> 32);
-		System.out.println("popping " + s);
+//		System.out.println("popping " + s);
 	}
 
 	void push(Solution s) {
