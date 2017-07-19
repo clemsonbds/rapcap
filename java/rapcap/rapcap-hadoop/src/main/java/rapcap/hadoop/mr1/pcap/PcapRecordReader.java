8550b86d5ba43ee3a9bbfa2a81233f0128a3c22e
Jason Anderson <jwa2@clemson.edu>
2017-07-19 09:54:51 -0400

    more debugging

--- java/rapcap/rapcap-hadoop/src/main/java/rapcap/hadoop/mr1/pcap/PcapRecordReader.java
+++ java/rapcap/rapcap-hadoop/src/main/java/rapcap/hadoop/mr1/pcap/PcapRecordReader.java
@@ -2,6 +2,7 @@ package rapcap.hadoop.mr1.pcap;
 
 import java.io.DataInputStream;
 import java.io.IOException;
+import java.util.Random;
 
 import org.apache.hadoop.fs.Seekable;
 import org.apache.hadoop.io.LongWritable;
@@ -12,14 +13,17 @@ import net.ripe.hadoop.pcap.PcapReader;
 
 public class PcapRecordReader extends net.ripe.hadoop.pcap.io.reader.PcapRecordReader {
 
+	private int ID;
+
 	public PcapRecordReader(PcapReader pcapReader, long start, long end, Seekable baseStream, DataInputStream stream,
 			Reporter reporter) throws IOException {
 		super(pcapReader, start, end, baseStream, stream, reporter);
+		ID = new Random().nextInt(99);
 	}
 
 	@Override
 	public boolean next(LongWritable key, ObjectWritable value) throws IOException {
-		System.out.println("rapcap: reading header at position " + getPos());
+		System.out.println("rapcap: reader " + ID + " reading header at position " + getPos());
 		return super.next(key, value);
 	}
 
