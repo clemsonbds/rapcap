414893f4aef79ea89c651ea78035f8e31d5cc727
Jason Anderson <jwa2@clemson.edu>
2017-07-31 12:36:32 -0400

    Modified LzoWordCount to use ToolRunner.

--- java/rapcap/rapcap-hadoop/src/main/java/example/LzoWordCount.java
+++ java/rapcap/rapcap-hadoop/src/main/java/example/LzoWordCount.java
@@ -1,66 +1,87 @@
 package example;
 
 import java.io.IOException;
+import java.util.Iterator;
 import java.util.StringTokenizer;
 
-import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.BytesWritable;
 import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
-import org.apache.hadoop.mapreduce.Job;
-import org.apache.hadoop.mapreduce.Mapper;
-import org.apache.hadoop.mapreduce.Reducer;
-import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
-import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapred.FileInputFormat;
+import org.apache.hadoop.mapred.FileOutputFormat;
+import org.apache.hadoop.mapred.JobClient;
+import org.apache.hadoop.mapred.JobConf;
+import org.apache.hadoop.mapred.MapReduceBase;
+import org.apache.hadoop.mapred.Mapper;
+import org.apache.hadoop.mapred.OutputCollector;
+import org.apache.hadoop.mapred.Reducer;
+import org.apache.hadoop.mapred.Reporter;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
 
-import rapcap.hadoop.mr1.lzo.LzoTextInputFormat;
+import rapcap.hadoop.mr1.lzo.LzoFileInputFormat;
 
-public class LzoWordCount {
+public class LzoWordCount extends Configured implements Tool {
 
-  public static class TokenizerMapper
-       extends Mapper<Object, Text, Text, IntWritable>{
+	public static class TokenizerMapper extends MapReduceBase implements Mapper<Object, Text, Text, IntWritable>{
 
-    private final static IntWritable one = new IntWritable(1);
-    private Text word = new Text();
+		private final static IntWritable one = new IntWritable(1);
+		private Text word = new Text();
 
-    public void map(Object key, Text value, Context context
-                    ) throws IOException, InterruptedException {
-      StringTokenizer itr = new StringTokenizer(value.toString());
-      while (itr.hasMoreTokens()) {
-        word.set(itr.nextToken());
-        context.write(word, one);
-      }
-    }
-  }
+		public void map(Object key, Text value, OutputCollector<Text, IntWritable> output, Reporter reporter)
+				throws IOException {
+			StringTokenizer itr = new StringTokenizer(value.toString());
+			while (itr.hasMoreTokens()) {
+				word.set(itr.nextToken());
+				output.collect(word, one);
+			}
 
-  public static class IntSumReducer
-       extends Reducer<Text,IntWritable,Text,IntWritable> {
-    private IntWritable result = new IntWritable();
+		}
+	}
 
-    public void reduce(Text key, Iterable<IntWritable> values,
-                       Context context
-                       ) throws IOException, InterruptedException {
-      int sum = 0;
-      for (IntWritable val : values) {
-        sum += val.get();
-      }
-      result.set(sum);
-      context.write(key, result);
-    }
-  }
+	public static class IntSumReducer extends MapReduceBase implements Reducer<Text,IntWritable,Text,IntWritable> {
+		private IntWritable result = new IntWritable();
 
-  public static void main(String[] args) throws Exception {
-    Configuration conf = new Configuration();
-    Job job = Job.getInstance(conf, "word count");
-    job.setJarByClass(LzoWordCount.class);
-    job.setInputFormatClass(LzoTextInputFormat.class);
-    job.setMapperClass(TokenizerMapper.class);
-    job.setCombinerClass(IntSumReducer.class);
-    job.setReducerClass(IntSumReducer.class);
-    job.setOutputKeyClass(Text.class);
-    job.setOutputValueClass(IntWritable.class);
-    FileInputFormat.addInputPath(job, new Path(args[0]));
-    FileOutputFormat.setOutputPath(job, new Path(args[1]));
-    System.exit(job.waitForCompletion(true) ? 0 : 1);
-  }
+		public void reduce(Text key, Iterator<IntWritable> values, OutputCollector<Text, IntWritable> output,
+				Reporter reporter) throws IOException {
+			int sum = 0;
+
+			while (values.hasNext())
+				sum += values.next().get();
+
+			result.set(sum);
+			output.collect(key, result);
+
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		int res = ToolRunner.run(new JobConf(), new ParallelDecompress(), args);
+		System.exit(res);
+	}
+
+	public int run(String[] args) throws Exception {
+		JobConf job = (JobConf)this.getConf();
+
+		FileInputFormat.addInputPath(job, new Path(args[0]));
+		FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+		job.setJobName("LZO word count");
+
+		job.setJarByClass(LzoWordCount.class);
+		job.setMapperClass(TokenizerMapper.class);
+		job.setReducerClass(IntSumReducer.class);
+
+		job.setInputFormat(LzoFileInputFormat.class);
+
+		job.setOutputKeyClass(LongWritable.class);
+		job.setOutputValueClass(BytesWritable.class);
+
+		job.setNumReduceTasks(1);
+		JobClient.runJob(job);
+		return 0;
+	}
 }
